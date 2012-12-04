3bcd5755c6aa6ee8c82ff2ca35b641de5d0b07f0
Robert Flack <flackr@gmail.com>
2012-12-03 21:07:52 -0500

    Implement the rest of the basic lobby functionality. Fixes #2.
    This adds game details to the lobby game list. The list of games can be queries from the /list url. The lobby pings the game server on a regular interval.

--- lobby-server/lobby-server.js
+++ lobby-server/lobby-server.js
@@ -2,11 +2,16 @@
  * Lobby server providing game listings for aribtrary games.
  */
 
-var port = 8000;
+var port = 9999;
 var http = require('http'),
     fs = require('fs'),
     path = require('path'),
     ws = require('websocket');
+
+// The folder to serve content out of.
+var www = '../lobby';
+// The default page to serve if no page is specified.
+var defaultHtml = 'index.html';
 
 var lobby = {};
 
@@ -30,9 +35,50 @@ lobby.Server = function() {
   Server.prototype = {
 
     onHttpRequest: function(request, response) {
-      console.log('Http request');
-      response.writeHead(200, { 'Content-Type': 'text/plain' });
-      response.end('Not implemented yet.');
+      function getContentType(filePath) {
+        if (filePath.substr(filePath.length - 5) == '.html')
+          return 'text/html';
+        if (filePath.substr(filePath.length - 4) == '.css')
+          return 'text/css';
+        if (filePath.substr(filePath.length - 3) == '.js')
+          return 'text/javascript';
+        return 'text/plain';
+      }
+
+      var separatorIndex = request.url.indexOf('?');
+      if (separatorIndex == -1)
+        separatorIndex = request.url.length;
+      var requestPath = request.url.slice(0, separatorIndex);
+      var requestQuery = request.url.slice(separatorIndex);
+      if (requestPath == '/search') {
+        // TODO(flackr): Handle search queries.
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end('Not implemented yet.');
+      } else if (requestPath == '/list') {
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify({'games': games}));
+      } else {
+        if (requestPath == '/')
+          requestPath = '/' + defaultHtml;
+        var filePath = www + requestPath.replace('../', './');
+        path.exists(filePath, function(exists) {
+          if (!exists) {
+            response.writeHead(404);
+            response.end();
+            return;
+          }
+          fs.readFile(filePath, function(error, content) {
+            if (error) {
+              response.writeHead(500);
+              response.end();
+              return;
+            }
+
+            response.writeHead(200, {'Content-Type': getContentType(filePath)});
+            response.end(content);
+          });
+        });
+      }
     },
 
     /**
@@ -40,34 +86,66 @@ lobby.Server = function() {
      * @param request The request object.
      */
     onWSRequest: function(request) {
-      console.log('Websocket connection received');
       var connection = request.accept('game-protocol', request.origin);
       this.setupGame(connection);
     },
 
     setupGame: function(connection) {
 
+      var self = this;
       var game;
+      var pingStart = 0;
+      var pingInterval;
 
+      var ping = function() {
+        if (pingStart == 0) {
+          pingStart = Date.now();
+          connection.sendUTF(JSON.stringify({type:'ping'}));
+        }
+      };
+
+      // TODO(flackr): We should close a connection after a timeout if it
+      // fails to register a game.
       connection.on('message', function(message) {
-        console.log('Received message: ' + message.utf8Data);
-        if (message.type == 'utf8') {
+        if (message.type != 'utf8') {
+          connection.close();
+          return;
+        }
+
+        var json;
+        try {
+          // TODO(flackr): The entire message may not fit in one packet. We
+          // should aggregate messages if we plan to have longer messages.
+          json = JSON.parse(message.utf8Data);
+        } catch (e) {
+          connection.close();
+          return;
+        }
+        if (json.type) {
           if (!game) {
-            // Before a game is started we only care about register.
-            if (message.utf8Data.slice(0, 9)  == 'register ') {
-              game = JSON.parse(message.utf8Data.slice(9));
-              // Add game to games list.
-              games.push(game);
-              connection.sendUTF('OK');
+            if (json.type == 'register') {
+              game = json.details;
+              game.publicAddress = connection.remoteAddress;
+              game.ping = undefined;
+              pingInterval = setInterval(ping, 10000);
+              self.onGameCreated(game);
             }
           } else {
-            // Handle messages after a game has been started.
-
+            if (json.type == 'update') {
+              for (var field in json.details) {
+                game[field] = json.details[field];
+              }
+            } else if (json.type == 'pong') {
+              game.ping = Date.now() - pingStart;
+              pingStart = 0;
+            }
           }
         }
       });
 
       connection.on('close', function(reasonCode, description) {
+        if (pingInterval)
+          clearTimeout(pingInterval);
         var index;
         if (game && (index = games.indexOf(game)) != -1) {
           // Remove game from list.
@@ -75,6 +153,10 @@ lobby.Server = function() {
         }
       });
 
+    },
+
+    onGameCreated: function(game) {
+      games.push(game);
     },
 
   };
--- server/lobby-game-host.js
+++ server/lobby-game-host.js
@@ -10,26 +10,46 @@ lobby.Host = function() {
     this.ws.onclose = this.connectionLost.bind(this);
     this.ws.onmessage = this.lobbyMessageReceived.bind(this);
     this.ws.onerror = this.onError.bind(this);
+    this.gameInfo = {
+      gameId: 'default',
+      name: 'Default',
+      description: 'This is the default game description',
+      status: 'awaiting_players',
+      accepting: true,
+      observable: true,
+      password: '',
+    };
   }
 
   Host.prototype = {
 
     registerServer: function(evt) {
       console.log('Connected, registering server');
-      this.ws.send('register {}');
+      this.ws.send(JSON.stringify({type: 'register', details: this.gameInfo}));
     },
 
     connectionLost: function(evt) {
-      alert('Connection to lobby lost');
+      console.log('Connection to lobby lost');
     },
 
     lobbyMessageReceived: function(evt) {
-      console.log('Received ' + evt.data);
+      try {
+        var json = JSON.parse(evt.data);
+        if (json.type == 'ping') {
+          this.ws.send(JSON.stringify({type: 'pong'}));
+        }
+      } catch(e) {
+        this.ws.close();
+      }
+    },
+
+    updateLobby: function() {
+      this.ws.send(JSON.stringify({type: 'update', details: this.gameInfo}));
     },
 
     onError: function(evt) {
       console.log('Error: ' + evt.data);
-    }
+    },
   };
 
   return Host;
