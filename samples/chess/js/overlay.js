c23a32ce1df72de2727b8ba3bb5973293ae66de4
Kevin Ellis <kevers@chromium.org>
2012-12-05 14:25:48 -0500

    Add overlay for chess game lobby.

---
+++ samples/chess/js/overlay.js
@@ -0,0 +1,122 @@
+
+
+Overlay = (function() {
+
+  var registry_ = {};
+
+  var activeOverlay_ = null;
+
+  function Overlay(name) {
+    this.initialize(name);
+  }
+
+  function getElement(name) {
+    return $(name + '-overlay');
+  }
+
+  Overlay.prototype = {
+
+     name: null,
+
+     initialize: function(name) {
+       this.name = name;
+     },
+
+     show: function() {
+       Overlay.show(this.name);
+     },
+
+     close: function() {
+       Overlay.dismiss(this.name);
+     },
+  }
+
+  /**
+   * Displays an overlay.
+   * @param {string} name Registered name of the overlay.
+   */
+  Overlay.show = function(name) {
+    activeOverlay_ = registry_[name];
+    var element = getElement(name);
+    // TODO: fade transition.
+    element.hidden = false;
+    element.parentNode.hidden = false;
+  }
+
+  Overlay.dismiss = function(name) {
+    var element = getElement(name);
+    // TODO: fade transition.
+    element.hidden = true;
+    element.parentNode.hidden = true;
+  }
+
+  Overlay.register = function(name, dialog) {
+    registry_[name] = dialog;
+  }
+
+  Overlay.getInstance = function(name) {
+    return registry_[name];
+  }
+
+  return Overlay;
+
+})();
+
+/**
+ * The chess lobby shows a list of games and provides the option for creating
+ * a new game.
+ */
+function ChessLobbyOverlay() {
+  Overlay.apply(this, ['chess-lobby']);
+}
+
+ChessLobbyOverlay.prototype = {
+  __proto__: Overlay.prototype,
+
+  initialize: function(name) {
+    Overlay.prototype.initialize.call(this, name);
+
+    var gameLobby = $('chess-lobby-list');
+
+    gameLobby.setFilter({name: 'chess'});
+
+    var lobbyUrl = gameLobby.getUrl();
+    var index = lobbyUrl.indexOf('://');
+    if (index > 0) {
+      lobbyUrl = lobbyUrl.substring(index + 3);
+    }
+    var lobbyUrl = lobbyUrl.split(':');
+
+    $('chess-lobby-url').value = lobbyUrl[0];
+    if (lobbyUrl.length > 0)
+    $('chess-lobby-port').value = lobbyUrl[1];
+
+    $('host-new-chess-game').addEventListener('click', this.onNewGame.bind(this));
+    $('refresh-game-list').addEventListener('click', this.onRefresh.bind(this));
+    $('chess-game-list-close').addEventListener('click', this.close.bind(this));
+
+  },
+
+  onNewGame: function(event) {
+    
+    // TODO: Popup dialog to insert game details.
+    // Create new game in Dialogs onCommit.
+
+    $('host-new-chess-game').disabled = true;
+    chess.createGame($('chess-lobby-url').value, $('chess-lobby-port').value);
+  },
+
+  onRefresh: function(event) {
+    $('chess-lobby-list').refresh();
+  },
+
+  show: function() {
+    $('host-new-chess-game').disabled = !!window.server;
+    Overlay.show(this.name);
+  }
+
+};
+
+window.addEventListener('load', function() {
+  Overlay.register('chess-lobby', new ChessLobbyOverlay());
+}, false);
