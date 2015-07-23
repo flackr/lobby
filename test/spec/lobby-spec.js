3a34f43a1b92ca8b5d1690cbbff61fe498b4b3f5
Robert Flack <flackr@gmail.com>
2015-07-23 01:00:38 -0400

    Added WebRTC mock to speed up testing.

--- test/spec/lobby-spec.js
+++ test/spec/lobby-spec.js
@@ -13,6 +13,7 @@ describe("lobby.Lobby", function() {
   beforeEach(function() {
     // Mock out communication, then run server
     installWebSocketMock();
+    installWebRTCMock();
     lobbyApi = new lobby.LobbyApi(new lobby.WebSocketSignalingClient('ws://localhost:'+testPort));
     server = new Server(testPort);
     originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
@@ -29,10 +30,25 @@ describe("lobby.Lobby", function() {
     var session;
     var sessionId;
     var hostRtcConnection;
+    var hostDataChannel;
+    var hostDataChannelReady;
 
     beforeEach(function(done) {
+      hostDataChannelReady = function() {};
+      hostDataChannel = null;
+      callback = null;
       session = lobbyApi.createSession(function(callback) {
         hostRtcConnection = new RTCPeerConnection(configuration, null);
+        hostRtcConnection.ondatachannel = function(e) {
+          hostDataChannel = e.channel;
+          if (hostDataChannel.readyState == 'open') {
+            hostDataChannelReady();
+          } else {
+            hostDataChannel.onopen = function() {
+              hostDataChannelReady();
+            }
+          }
+        };
         return hostRtcConnection;
       });
       session.addEventListener('open', function(id) {
@@ -46,7 +62,6 @@ describe("lobby.Lobby", function() {
       
       var clientRtcPeerConnection;
       var clientDataChannel;
-      var hostDataChannel;
       
       beforeEach(function(done) {
         clientRtcPeerConnection = new RTCPeerConnection(configuration, null);
@@ -55,9 +70,13 @@ describe("lobby.Lobby", function() {
         clientDataChannel.onopen = function() {
           client.close();
           expect(hostRtcConnection).toBeTruthy();
-          hostRtcConnection.ondatachannel = function(e) {
-            hostDataChannel = e.channel;
-            hostDataChannel.onopen = done;
+          if (!hostDataChannel || hostDataChannel.readyState != 'open') {
+            hostDataChannelReady = function() {
+              hostDataChannelReady = null;
+              done();
+            };
+          } else {
+            done();
           }
         };
       });
@@ -69,12 +88,13 @@ describe("lobby.Lobby", function() {
           expect(e.data).toBe(serverMsg);
           done();
         });
-        clientDataChannel.send(clientMsg);
 
         hostDataChannel.addEventListener('message', function(e) {
           expect(e.data).toBe(clientMsg);
           hostDataChannel.send(serverMsg);
         });
+
+        clientDataChannel.send(clientMsg);
       });
       
       it("should detect the client disconnecting", function(done) {
